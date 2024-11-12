dfe9f91ee1ce421ffd511cf6dc2a72d1542dfc6e
Sonu <138742493+sonuokk@users.noreply.github.com>
2024-11-12 23:46:47 +0530

    AVL tree code added

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -39,26 +39,22 @@ public:
     Node* rightRotate(Node* y) {
         Node* x = y->left;
         Node* T2 = x->right;
-
         x->right = y;
         y->left = T2;
 
         y->height = max(height(y->left), height(y->right)) + 1;
         x->height = max(height(x->left), height(x->right)) + 1;
-
         return x;
     }
 
     Node* leftRotate(Node* x) {
         Node* y = x->right;
         Node* T2 = y->left;
-
         y->left = x;
         x->right = T2;
 
         x->height = max(height(x->left), height(x->right)) + 1;
         y->height = max(height(y->left), height(y->right)) + 1;
-
         return y;
     }
 
@@ -66,28 +62,25 @@ public:
         if (node == NULL)
             return new Node(data);
 
-        if (data < node->data)
+        if (data < node->data) {
             node->left = insert(node->left, data);
-        else if (data > node->data)
+        } else if (data > node->data) {
             node->right = insert(node->right, data);
-        else
+        } else {
             return node;
+        }
 
         node->height = 1 + max(height(node->left), height(node->right));
-
         int balance = getBalance(node);
 
         if (balance > 1 && data < node->left->data)
             return rightRotate(node);
-
         if (balance < -1 && data > node->right->data)
             return leftRotate(node);
-
         if (balance > 1 && data > node->left->data) {
             node->left = leftRotate(node->left);
             return rightRotate(node);
         }
-
         if (balance < -1 && data < node->right->data) {
             node->right = rightRotate(node->right);
             return leftRotate(node);
